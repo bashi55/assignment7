4efcc0199e3e38d384867c4459bcf31ea7f1249a
bashi55 <154985516+bashi55@users.noreply.github.com>
2024-03-28 17:34:11 +0530

    Update EmployeeBonus.java

--- EmployeeBonus.java
+++ EmployeeBonus.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class EmployeeBonus {
+public class EmployeeBonusCalculator {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
@@ -12,19 +12,23 @@ public class EmployeeBonus {
         
         int yearsOfService = currentYear - joinYear;
         
-        int bonus = 0;
+        int bonusAmount = 0;
         
         if (yearsOfService > 5) {
-            bonus = 5000;
+            bonusAmount = 5000;
         } else if (yearsOfService >= 3 && yearsOfService <= 5) {
-            bonus = 3000;
+            bonusAmount = 3000;
         } else {
             System.out.println("No bonus awarded.");
         }
         
-        if (bonus > 0) {
-            System.out.println("Congratulations! You've been awarded a bonus of Rs. " + bonus + "/-");
+        if (bonusAmount > 0) {
+            System.out.println("Congratulations! You've been awarded a bonus of Rs. " + bonusAmount + "/-");
         }
+        
+        scanner.close();
+    }
+}
         
         scanner.close();
     }
